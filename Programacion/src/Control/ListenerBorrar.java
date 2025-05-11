f98f0ad1410c209e677f3b1b7f9bf0bb102263d4
Alejandro <alexrabosomartos@gmail.com>
2025-05-11 18:42:02 +0200

    Listener Borrar

--- Programacion/src/Control/ListenerBorrar.java
+++ Programacion/src/Control/ListenerBorrar.java
@@ -3,14 +3,41 @@ package Control;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JOptionPane;
+
+import modelo.Actividad;
 import vistas.InformacionActividadMonitor;
 
 public class ListenerBorrar implements ActionListener {
-
+	Actividad Act;
+	int idActividad;
+	int idMonitor;
+	String nombreActividad;
+	String descripcionActividad;
+	int usuariosMaximos;
+	int usuariosInscritos;
+	String fechaActividad;
+	String HoraActividad;
+	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-
+		idActividad = Act.getIdActividad();
+		idMonitor = Act.getIdMonitor();
+		nombreActividad = Act.getNombreActividad();
+		descripcionActividad = Act.getDescripcionActividad();
+		usuariosMaximos = Act.getUsuariosMaximos();
+		usuariosInscritos = Act.getUsuariosInscritos();
+		fechaActividad = Act.getFechaActividad();
+		HoraActividad = Act.getHoraActividad();
+		
+		Act  = new Actividad(idActividad, idActividad, HoraActividad, HoraActividad, idActividad, idActividad, HoraActividad, 
+				HoraActividad); 
+		int Confirmacion = JOptionPane.showConfirmDialog(null, "¿Estás seguro?", "Confirmar", JOptionPane.YES_NO_OPTION);
+		
+		if(Confirmacion == JOptionPane.YES_OPTION){
+			Act.eliminarActividad();
+		}
 	}
 
 }
