34bac535ccd8cbf1e90fffa93af6433030df3d23
MariaQGarrido <mariaqingxuan@gmail.com>
2025-05-25 14:05:53 +0200

    cambios

--- Programacion/src/main/java/com/jamonasiatico/modelo/BBDD.java
+++ Programacion/src/main/java/com/jamonasiatico/modelo/BBDD.java
@@ -14,7 +14,7 @@ public class BBDD {
 	private String url = "jdbc:mysql://localhost/base_integrador";
 
 	private String usuarioBBDD = "root";
-	private String passwdBBDD = "1234567";
+	private String passwdBBDD = "Tote2005@";
 
 	private Connection con;
 
